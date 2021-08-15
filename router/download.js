4bb18a32a983ca5aad83d2c13408d9bb5ecc61ae
PrarthanaShanbhag <prarthanaushanbhag@gmail.com>
2021-08-15 18:01:56 +0530

    inshare-easy file tranfer

--- router/download.js
+++ router/download.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
 const File = require('../models/file')
 const path = require('path')
-const dir=path.join(__dirname,'..')
+const dir=path.join(__dirname,'..','..')
+const uploads = `${dir}`;
+
 router.get('/:uuid',async(req,res)=>{
     try{
         const file =await File.findOne({uuid:req.params.uuid})
         if(!file){
             return res.render('404',{err :'Download link expired'});
         }
-        console.log(__dirname)
-        const filePath=`${dir}/${file.path}`
+        console.log(uploads)
+        const filePath=`${uploads}/${file.path}`
         console.log(filePath)
         res.download(filePath);
     }
